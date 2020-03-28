65d00b5adafd4e62d5c23ba5e3b31322d2b60b2a
Micros231 <kseromenko@gmail.com>
2020-03-28 09:26:47 +0200

    add manifest objecct and controller

---
+++ Assets/ABManagerSystem/Core/Manifest/ABManifest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ABManagerCore.Manifest
+{
+    [Serializable]
+    public class ABManifest
+    {
+        public string Version { get => _version; internal set => _version = value; }
+
+        [SerializeField]
+        private string _version;
+
+    }
+}
+
---
+++ Assets/ABManagerSystem/Editor/Controller/ABBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using ABManagerCore.Manifest;
+
+namespace ABManagerEditor.Controller
+{
+    public class ABBuilder
+    {
+        public void Build()
+        {
+            ABManifest manifest = new ABManifestBuilder().Version("0").Build();
+
+            CreateFileManifest(manifest);
+        }
+
+        private void CreateFileManifest(ABManifest manifest)
+        {
+            if (manifest == null)
+                throw new NullReferenceException("manifest object is null");
+            string jsonString = EditorJsonUtility.ToJson(manifest);
+            using (var streamWriter = File.CreateText(Path.Combine(Application.dataPath, "test-manifest.json")))
+            {
+                streamWriter.Write(jsonString);
+            }
+        }
+
+
+    }
+}
+
